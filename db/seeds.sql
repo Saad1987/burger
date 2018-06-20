6a87086e778f8304a1a8e2e330dfc709f7dc2c55
Saad1987 <saad.mourtada@gmail.com>
2018-06-20 18:17:17 -0500

    final commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese beef burger', true);
+INSERT INTO burgers (burger_name) VALUES ('fish-O-filet burger');
+INSERT INTO burgers (burger_name) VALUES ('chicken burger');
